a622bfc5568341045a25ebf25dee150c5faac48d
Sébastien Doucet <sebdoucet@gmail.com>
2022-01-19 16:30:33 -0500

    Add isVisible(rect: DOMRect)

--- src/utilities/position.ts
+++ src/utilities/position.ts
@@ -1,6 +1,8 @@
 
-export function isVisible(el: HTMLElement): boolean {
-    const rect = el.getBoundingClientRect();
+export function isVisible(rect: DOMRect): boolean;
+export function isVisible(el: HTMLElement): boolean;
+export function isVisible(elOrRect: HTMLElement | DOMRect): boolean {
+    const rect = elOrRect instanceof HTMLElement ? elOrRect.getBoundingClientRect(): elOrRect;
     return rect.top >= 0
         && rect.left >= 0
         && rect.bottom <= document.documentElement.clientHeight
