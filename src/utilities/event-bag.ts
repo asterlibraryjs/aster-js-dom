aadc008d0345d4d2701b4c53e5ded8d3605f09e5
Sebastien Doucet <sebdoucet@gmail.com>
2022-05-09 08:03:03 -0400

    EventBag and package updates

---
+++ src/utilities/event-bag.ts
@@ -0,0 +1,34 @@
+import { Disposable, IDisposable } from "@aster-js/core";
+import { on } from "./events";
+
+/** Manage events for a specific target. Very usefull for component event management */
+export class EventBag extends Disposable {
+    private readonly _events: IDisposable[] = [];
+
+    get size(): number { return this._events.length; }
+
+    constructor(
+        private readonly _target: EventTarget
+    ) {
+        super();
+    }
+
+    add<T extends Event>(type: string, callback: (evt: T) => void, options?: AddEventListenerOptions | boolean): this;
+    add(type: string, callback: EventListenerOrEventListenerObject, options?: AddEventListenerOptions | boolean): this;
+    add(type: string, callback: EventListenerOrEventListenerObject, options?: AddEventListenerOptions | boolean): this {
+        this._events.push(
+            on(this._target, type, callback, options)
+        );
+        return this;
+    }
+
+    clear(): void {
+        IDisposable.safeDisposeAll(
+            this._events.splice(0)
+        );
+    }
+
+    protected dispose(): void {
+        this.clear();
+    }
+}
